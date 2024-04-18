f75bf883db0b1cebfef9ef190dc14cbb3ff86846
Esquimalt Atom Smashers (Alienware) <esqatomsmashers@gmail.com>
2024-04-18 16:03:24 -0700

    Test commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CustomSubsystemBase.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class CustomSubsystemBase {
+
+    private Telemetry telemetry;
+
+    public CustomSubsystemBase(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveBaseSubsystem.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DriveBaseSubsystem extends CustomSubsystemBase {
+
+    public DriveBaseSubsystem(Telemetry telemetry) {
+        super(telemetry);
+    }
+}
